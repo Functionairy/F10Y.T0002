ec1e62f45992d002ac0a438d3355d4f452a7f023
SURFEIT\David <david.coats@gmail.com>
2025-03-20 23:02:11 -0700

    Normalization of deviance.

---
+++ source/F10Y.T0002/Code/Attributes/FunctionsMarkerAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using F10Y.T0000;
+
+
+namespace F10Y.T0002
+{
+    /// <summary>
+    /// Attribute indicating an interface contains function instances. (i.e. default implementation methods on an interface).
+    /// </summary>
+    /// <remarks>
+    /// The marker attribute is useful for surveying function interfaces and building a catalogue of functions.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Interface, // Functions are always default implementation methods on an interface.
+        AllowMultiple = false, // No.
+        Inherited = false // Make the inheritor also use the marker.
+    )]
+    [MarkerAttributeMarker]
+    public class FunctionsMarkerAttribute : Attribute
+    {
+        /// <summary>
+        /// Allows specifying that an interface is *not* a functionality interface.
+        /// This is useful for marking interfaces that end up canonical functionality code file locations, but are not functionality interfaces.
+        /// </summary>
+        public bool Is_Functionality { get; }
+
+
+        public FunctionsMarkerAttribute(
+            bool is_Functionality = true)
+        {
+            this.Is_Functionality = is_Functionality;
+        }
+    }
+}
